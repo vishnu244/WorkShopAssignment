8267806f8ef89b01ade1b394251ddef5650d0e5f
vishnu244 <vishnunali244@gmail.com>
2022-05-26 13:18:19 +0530

    [Vishnu]- Monthly payment

---
+++ Monthly_Payment/Monthlypayment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monthly_Payment
+{
+    public class Monthlypayment
+    {
+        public static void Calculate()
+        {
+            Console.WriteLine("Enter the principle");
+            double P = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Number of Years");
+            double Y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Rate of Interest");
+            double R = Convert.ToInt32(Console.ReadLine());
+
+            double n = 12 * Y;
+            double r = R / (12 * 100);
+
+            //double x = Math.Pow((1 + r), -n);
+            //Console.WriteLine(x);
+
+            double Payment = (P * r) / (1 - Math.Pow((1 + r), -n));
+            Console.WriteLine("Monthly payment that has to be paid : " + Payment);
+        }
+    }
+}
---
+++ Monthly_Payment/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Monthly_Payment;
+
+Console.WriteLine("Welcome to Monthly Payment Assignment!");
+
+
+Monthlypayment.Calculate();
